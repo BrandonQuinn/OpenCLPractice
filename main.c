d30b4623675b6b3d9ab86cf6ffc5b1133c472f26
Brandon Quinn <brandonquinnn@gmail.com>
2017-11-04 15:05:12 +1100

    Program

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <CL\cl.h>
 
+#define PROGRAM "testkernel.cl"
+#define ADD_KERNEL "add"
+#define SUB_KERNEL "sub"
+#define MULT_KERNEL "mult"
+#define DIV_KERNEL "div"
+
 // Return all the platform ids on this machine.
 cl_platform_id* GetPlatforms(
 		cl_uint* numPlatforms) 
@@ -82,9 +88,8 @@ cl_device_id GetFirstGPU(cl_platform_id platform)
 // Print out the name and vendor of the ginve device.
 void PrintDeviceInfo(cl_device_id device)
 {
-	size_t dataSize;
 	cl_uint error;
-	char data[4096];
+	char data[4096]; size_t dataSize;
 
 	error = clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(data), data, &dataSize);
 	printf("\nSelected Device: %s\n", data);
@@ -108,6 +113,59 @@ cl_context CreateContext(cl_device_id device)
 	return context;
 }
 
+// Read in the program source and create a cl_program from it.
+cl_program CreateProgram(cl_context context)
+{
+	cl_program program;
+	cl_uint error;
+	FILE* handle; size_t programSize;
+	char* programBuf;
+
+	// read the program source
+	handle = fopen(PROGRAM, "r");
+	if (handle == NULL) {
+		printf("Could not open program file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fseek(handle, 0, SEEK_END);
+	programSize = ftell(handle);
+	rewind(handle);
+	programBuf = (char*) malloc(sizeof(char) * programSize);
+	programBuf[programSize] = '\0';
+	fread(programBuf, sizeof(char), programSize, handle);
+	fclose(handle);
+
+	program = clCreateProgramWithSource(context, 1, (const char**) & programBuf, &programSize, &error);
+	
+	if (error != CL_SUCCESS) {
+		printf("Could not create program with source\n");
+		exit(EXIT_FAILURE);
+	}
+
+	free(programBuf);
+	return program;
+}
+
+// Build the program and if it fails print out the log information.
+void BuildProgram(cl_program program, cl_device_id device)
+{
+	cl_uint error, logSize;
+	char* programLog;
+
+	error = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+
+	if (error < 0) {
+		clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &logSize);
+		programLog = (char*)malloc(logSize + 1);
+		programLog[logSize] = '\0';
+		clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize + 1, programLog, NULL);
+		printf("%s\n", programLog);
+		free(programLog);
+		exit(EXIT_FAILURE);
+	}
+}
+
 int main() 
 {
 	cl_platform_id* platforms;
@@ -115,6 +173,7 @@ int main()
 	cl_uint numPlatforms;
 	cl_device_id curDevice;
 	cl_context context;
+	cl_program program;
 
 	// platform
 	platforms = GetPlatforms(&numPlatforms);
@@ -125,11 +184,16 @@ int main()
 	curDevice = GetFirstGPU(curPlatform);
 	PrintDeviceInfo(curDevice);
 
-	// context
-	context = CreateContext(curDevice);
-	
+	context = CreateContext(curDevice);	
+	program = CreateProgram(context);
+	BuildProgram(program, curDevice);
+
+
 
 	getchar();
+
+	clReleaseProgram(program);
+	clReleaseContext(context);
 	free(platforms);
 	return 0;
 }
